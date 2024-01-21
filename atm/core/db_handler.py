fc860526190cc6c6e94576900491919622e943c1
XieChengG <xiechenggang@yeah.net>
2024-01-21 11:48:25 +0800

    add select and update account data

--- atm/core/db_handler.py
+++ atm/core/db_handler.py
@@ -28,5 +28,27 @@ def file_execute(sql, **kwargs):
     sql_list = sql.split("where")
     print(sql_list)
 
+    # select 查询账户信息并返回账户信息数据
     if sql_list[0].startswith("select") and len(sql_list) > 1:  # select and where
         column, val = sql_list[1].strip().split("=")
+
+        if column == "account":
+            account_file = "%s/%s.json" % (db_path, val)
+            print(account_file)
+            if os.path.isfile(account_file):
+                with open(account_file, 'r') as f:
+                    account_data = json.load(f)
+                    return account_data
+            else:
+                exit("Account %s is not exist!" % val)
+
+    # update 更新账户数据
+    elif sql_list[0].startswith("update") and len(sql_list) > 1:  # update and where
+        column, val = sql_list[1].strip().split("=")
+        if column == "account":
+            account_file = "%s/%s.json" % (db_path, val)
+            if os.path.isfile(account_file):
+                account_data = kwargs.get('account_data')
+                with open(account_file, 'w') as f:
+                    json.dump(account_data, f)
+                return True
