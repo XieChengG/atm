4a0957a37d57edb21538d3a17f8431a73ecf3c7c
xiechenggang <xiechenggang1991@163.com>
2024-01-22 11:03:26 +0800

    user login function

--- atm/core/auth.py
+++ atm/core/auth.py
@@ -18,15 +18,24 @@ def acc_auth(account, password):
     if data['password'] == password:
         exp_time_stamp = time.mktime(time.strptime(data['expire_date'], "%Y-%m-%d"))
         if time.time() > exp_time_stamp:
-            print("Account [%s] has expired" %account)
+            print("Account [%s] has expired" % account)
         else:
             return data
     else:
         print("Account ID or password is incorrect")
 
 
-
-
-
-
-
+def acc_login(user_data, log_obj):
+    retry_count = 0
+    while user_data['is_authenticated'] is not True and retry_count < 3:
+        account = input("account:").strip()
+        password = input("password:").strip()
+        auth = acc_auth(account, password)
+        if auth:
+            user_data['is_authenticated'] = True
+            user_data['account_id'] = account
+            return auth
+        retry_count += 1
+    else:
+        log_obj.error("account [%s] attempts to login too many times" % account)
+        exit()
